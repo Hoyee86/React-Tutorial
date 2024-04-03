777e8f6335e8421f08d77540e7e7ea8a7b38a587
Hoyee86git config --global user.name Hoyee86 <azeezabdullahi264@gmail.com>
2024-04-03 13:05:12 +0100

    updating

---
+++ react-router/src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom"
+import About from "./About"
+import Footer from "./Footer"
+import Header from "./Header"
+import Home from "./Home"
+import Missing from "./Missing"
+import Nav from "./Nav"
+import NewPost from "./NewPost"
+import PostPage from "./PostPage"
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Header/>
+      <Nav/>
+      <Routes>
+        <Route  path="/" element={<Home/>}/>
+        <Route path="/about"  element={<About/>} />
+        <Route path="/post-page"  element={<PostPage/>} />
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
